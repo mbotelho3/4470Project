03dbf5117947f005916de44c7e6df46641a8a224
mbotelho3 <mluisax3@gmail.com>
2014-10-19 19:35:07 -0400

    new classes - image & tcomponent

---
+++ project/src/project/Thumbnail.java
@@ -0,0 +1,12 @@
+package project;
+
+import javax.swing.JComponent;
+
+public class Thumbnail extends JComponent {
+	private PComponent pc;
+	
+	public Thumbnail (PComponent pc){
+		this.pc = pc;
+	}
+	
+}
